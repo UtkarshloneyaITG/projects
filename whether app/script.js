5f04b1eced3c2209127d542fe735d71b6c1a2509
UtkarshloneyaITG <UtkarshLoniya>
2025-08-25 19:59:30 +0530

    whether app error fixed

--- whether app/script.js
+++ whether app/script.js
@@ -7,35 +7,57 @@ let preasure = document.querySelector('.Pressure')
 let Wind = document.querySelector('.Wind')
 let temperature = document.querySelector('.temperature')
 let description = document.querySelector('.description')
+let container = document.querySelector('.whether_app')
+let weathericon = document.querySelector('.weather-icon')
+let loader = document.querySelector('.center-loader')
 let objWether = {}
 
 async function whetherReturner(cityname) {
   try {
+    container.style.display = 'none'
+    loader.style.display = 'flex'
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=a173a9475981faa920d7213a714f3194&units=metric`)
     let whetherObj = await response.json()
     objWether = whetherObj
+    loader.style.display = 'none'
+    container.style.display = 'block'
   } catch (error) {
-    console.error(error);
-    return error
+    loader.style.display = 'none'
+    console.error('error', error)
+    return false
   }
-
+  return objWether
 }
 
 async function updater(name) {
   await whetherReturner(name)
-
-  if(city.value === '') {
-    error.innerText = 'please enter city name'
-    return
-  }
-  else error.innerText = ''
-
+    .then((value) => {
+      if (value.cod == 404) {
+        error.innerText = 'please enter correct city name'
+      }
+      else if (value == false) {
+        error.innerText = 'Somthing went wrong'
+      }
+      else if (value.cod == 400) {
+        error.innerText = 'please enter the city name'
+      }
+      else {
+        updaterNew()
+      }
+    })
+    .catch((value) => {
+      console.log(value)
+    })
+}
+function updaterNew() {
+  error.innerText = ''
   let obj = objWether
+  weathericon.src = `https://openweathermap.org/img/wn/${obj.weather[0].icon}.png`
   cityName.innerText = obj.name
   description.innerText = obj.weather[0].description
   temperature.innerText = `${obj.main.temp}ºC`
   Humidity.innerText = `${obj.main.humidity}%`
-  preasure.innerText =`${obj.main.pressure} mb`
+  preasure.innerText = `${obj.main.pressure} mb`
   Wind.innerText = `${obj.wind.speed} km/h`
-  console.log(obj)
+
 }
